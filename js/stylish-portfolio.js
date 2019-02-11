08033bc1b0c6c6964a5f926a4a7679156dd3d50f
clark-rob <clark.rob.g@gmail.com>
2019-02-11 12:21:55 -0500

    Image update
    HTML:
    - relocated .contact-me above footer
    - added footer image
    - changed image for 'coming soon'
    
    CSS:
    - styling update

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,14 +1,6 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-    $(this).toggleClass("active");
-  });
-
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
